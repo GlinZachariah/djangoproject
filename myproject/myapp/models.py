d5c774e97c28dc8e47fafb4d5ab7f153c4f77753
GlinZachariah <glincool.zac@gmail.com>
2018-08-27 21:47:07 +0530

    added post admin files

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -8,3 +8,6 @@ class Posts(models.Model):
     title = models.CharField(max_length=20)
     body= models.TextField();
     created_at = models.DateTimeField(default=datetime.now,blank=True )
+
+    def __str__(self):
+        return self.title;
